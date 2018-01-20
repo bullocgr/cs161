403a4ac9f3822f5ba8d2a59c0a3bf07d0e21288a
bullocgr <bullocgr@oregonstate.edu>
2018-01-19 18:05:39 -0800

    updated code

--- assignments161/assignment2/assignment2.cpp
+++ assignments161/assignment2/assignment2.cpp
@@ -11,9 +11,19 @@ int main () {
 	cout << "You walk up to a mysterious gate. You get scared but decide to go through it anyway. This begins your journey.\n";
 	cout << "When you come out on the other side you see stairs. Do you go down the stairs?\n";
 	cin >> m;
-//	m == 'y' || m == 'n'
-//follow the goat
+
 	if (m == 'y') {
+		cout << "You see 5 different paths. Choose one to go down. 1, 2, 3, 4, or 5." << endl;
+		cin >> x;
+	}
+	if (m == 'n') {
+		cout << "You turn back but the gate is closed. You are forced to go down the path." << endl;
+		cout << "Choose one to go down. 1, 2, 3, 4, or 5." << endl;
+		cin >> x;
+	}
+
+//follow the goat (path1)
+	if (x == 1) {
 		cout << "You go down the stairs and see a goat standing on its two hind legs. It asks you to follow it. Do you follow it? " << endl;
 		cin >> m;
 		if (m == 'y') {
@@ -26,19 +36,27 @@ int main () {
 				cout << "You wander off and get lost. You can't find your way back and end up starving." << endl;
 			}
 		}
-
 	}
 			
 
-//don't follow the goat
-	if (m == 'n') {
-		cout << "You turn to go back through the gate but you can't pass back through. You slowly descend the stairs." << endl;
-		cout << "At the bottom of the stairs you see a goat standing on its hind legs. It asks you to follow it." << endl;
+//don't follow the goat (path2)
+	if (x == 2) {
+		cout << "At the bottom of the stairs you see a hot dog. Do you eat the hot dog?" << endl;
 		cin >> m;
-		if (m == 'n') {
-			cout << "You don't follow the goat and leave. You end up falling down a trap. You look around and think you can climb out. Do you try to climb out?" << endl;
+		if (m == 'y') {
+			cout << "You eat the hot dog and a skeleton comes. He goes to shake your hand. Shake his hand?" << endl;
 			cin >> m;
-			}
+				if (m == 'y') {
+					cout << "You shake his hand and he smiles at you.\nHe tells you he can't get you out but that he will protect you in this world.\nSeems good enough for you." << endl;
+				}
+				if (m == 'n') {
+					cout << "He gets frustrated and becomes a spooky skeleton instead.\nThis scares you so badly that you die. RIP you." << endl;
+				}
+		}
+
+		else if (m == 'n') {
+			cout << "You don't eat the hot dog and walk away.\nYou end up falling down a trap. You look around and think you can climb out.\nDo you try to climb out?" << endl;
+			cin >> m;
 			if (m == 'y') {
 				cout << "You try to climb out and fall down. Too bad but you died." << endl;
 				}
@@ -60,7 +78,8 @@ int main () {
 						cout << "RIP" << endl;
 					}
 				}
-	}
+			}
+		}
 		
 	return 0;
 
